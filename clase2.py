97b16840969b59d8c9ee80ccb321d02f09ea5a63
isatabernero <isabeltabernerosoto2003@gmail.com>
2025-02-22 09:38:25 -0500

    hola que tal

---
+++ clase2.py
@@ -0,0 +1,29 @@
+#para ver el tipo de variable 
+texto = "ejemplo"
+print(type(texto))
+
+anio_nacimiento = 2003
+anio_actual = 2025
+
+# recibir variable 
+# declarar el método
+def calcularedad(nacimiento, actual):
+    edad = actual - nacimiento
+    return edad 
+
+# para llamar al método
+calcularedad(anio_nacimiento, anio_actual)
+
+
+print(calcularedad(anio_nacimiento, anio_actual))
+
+#para crear nuevo método
+def metodosumar():
+    anio = 2024 + 1
+    return anio
+
+def anioactual():
+    metodosumar()
+
+
+
---
+++ formula.py
@@ -0,0 +1,11 @@
+# recibir variable 
+# declarar el método
+
+
+def calcularedad(nacimiento, actual):
+    edad = actual - nacimiento
+    return edad 
+
+def numero_carnet(DNI):
+    carnet = DNI + 43
+    return carnet 
