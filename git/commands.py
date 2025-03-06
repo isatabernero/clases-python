97cfbd5ce0fa2b7b7d153c454bde949dd090d130
Rachel Carretero <rachel.Carretero@thecrownestate.co.uk>
2025-03-06 16:15:40 +0100

    apuntes básicos y reorganización de carpetas

--- git/commands.py
+++
@@ -1,5 +0,0 @@
-#comandos de git 
-
-print ("git add .") #para añadir todos los cambios que tenemos.
-print ("git checkout mirama") #ir a mi rama
-print("commit -m") #para subir/ guardar los cambios
